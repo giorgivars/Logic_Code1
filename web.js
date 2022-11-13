9def8c576e61f7a78575069558d2f826adb3e17c
giorgivars <118048305+giorgivars@users.noreply.github.com>
2022-11-13 21:34:02 +0400

    Add files via upload

---
+++ web.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+var http = require('http');
+
+app.get('/', function(request, response){
+	var getClientAddress = function (req) {
+	    return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+	        || req.connection.remoteAddress;
+	};
+	response.send(getClientAddress(request));
+});
+
+app.get('/api/ipaddress.json', function(request, response){
+	var getClientAddress = function (req) {
+	    return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+	        || req.connection.remoteAddress;
+	};
+	// response.send(getClientAddress(request));
+	response.json({userAgent:request.headers['user-agent'],method:request.method,fresh:request.fresh,xhr:request.xhr,protocol:request.protocol,ipAddress:getClientAddress(request),remoteAddress:request.connection.remoteAddress,forwarded:request.headers['x-forwarded-for']});
+});
+
+// Using built-in http library and public API from https://www.ipify.org/
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+  resp.on('data', function(ip) {
+    console.log("My public IP address is: " + ip);
+  });
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Listening on " + port))
